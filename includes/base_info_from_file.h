407fd72caa5f4399dca0cde7cb9fe3f8becb1ba4
Uliana <53272893+Uliana-So@users.noreply.github.com>
2022-05-04 17:53:17 +0300

    Update codestyle for gitHub (base_info_from_file.h)

--- includes/base_info_from_file.h
+++ includes/base_info_from_file.h
@@ -6,31 +6,31 @@ typedef struct s_vector
 	double		x;
 	double		y;
 	double		z;
-}				t_vector;
+}			t_vector;
 
 typedef struct s_color
 {
 	double		r;
 	double		g;
 	double		b;
-}				t_color;
+}			t_color;
 
 typedef struct s_base_info
 {
 	struct s_resolution	*resolution;
 	struct s_ambient	*ambient;
-}						t_base_info;
+}				t_base_info;
 
 typedef struct s_resolution
 {
-	int					width;
-	int					height;
-}						t_resolution;
+	int			width;
+	int			height;
+}				t_resolution;
 
 typedef struct s_ambient
 {
-	t_color		color;
-	double		ratio;
+	t_color			color;
+	double			ratio;
 }				t_ambient;
 
 typedef struct s_all_cam
@@ -38,13 +38,13 @@ typedef struct s_all_cam
 	struct s_camera		*cam;
 	struct s_all_cam	*next;
 	struct s_all_cam	*prev;
-}						t_all_cam;
+}				t_all_cam;
 
 typedef struct s_camera
 {
 	t_vector		location;
 	t_vector		dir;
 	double			fov;
-}					t_camera;
+}				t_camera;
 
 #endif
